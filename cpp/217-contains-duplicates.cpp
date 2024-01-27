b34aae2e8cc45648b0129a11c73c4966c1786a65
Dyana Rahhal <100005475+DyanaR@users.noreply.github.com>
2024-01-27 01:04:33 -0600

    Update 217-contains-duplicates.cpp

--- cpp/217-contains-duplicates.cpp
+++ cpp/217-contains-duplicates.cpp
@@ -1,13 +1,13 @@
-bool containsDuplicate(int* nums, int numsSize) {
-    for(int i = 0; i <= numsSize; i++){
-        for(int j = i+1; j< numsSize; j++){
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        for(int i = 0; i <= nums.size(); i++){
+        for(int j = i+1; j< nums.size(); j++){
             if(nums[i] == nums[j]){
-                return true
-            }
-            else{
-                return false;
+                return true;
             }
         }
     }
-    
-}
+    return false;
+    }
+};
